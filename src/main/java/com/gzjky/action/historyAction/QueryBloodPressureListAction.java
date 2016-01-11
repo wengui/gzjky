6197906b48fc7efdf29a1eacbdf6bd40fcb5a59f
yuting0787 <626808708@qq.com>
2016-01-11 17:44:40 +0800

    做成

--- src/main/java/com/gzjky/action/historyAction/QueryBloodPressureListAction.java
+++ src/main/java/com/gzjky/action/historyAction/QueryBloodPressureListAction.java
@@ -3,13 +3,17 @@ package com.gzjky.action.historyAction;
 import java.io.PrintWriter;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.math.NumberUtils;
 import org.apache.struts2.ServletActionContext;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.gzjky.action.acitonCommon.ModelMap;
+import com.gzjky.bean.extend.QueryBloodPressureInputBean;
 import com.gzjky.bean.extend.QueryBloodPressureOutputBean;
+import com.gzjky.dao.constant.CodeConstant;
 import com.gzjky.dao.readdao.BloodPressureHistoryReadMapper;
 import com.opensymphony.xwork2.ActionSupport;
 
@@ -37,13 +41,27 @@ public class QueryBloodPressureListAction extends ActionSupport{
 		
 		
 		try {
-			HttpServletResponse response=ServletActionContext.getResponse();  
-	        //以下代码从JSON.java中拷过来的  
-	        response.setContentType("text/html");  
-	        PrintWriter out;  
-	        out = response.getWriter();  
+
+			// 页面参数取得
+			HttpServletRequest request = ServletActionContext.getRequest();
 			
-	        bloodPressureList = bloodPressureHistoryReadMapper.selectBloodPressureByCondition("", "正常");
+			// 查询参数设定
+			QueryBloodPressureInputBean queryBloodPressureInputBean = new QueryBloodPressureInputBean();
+			queryBloodPressureInputBean.setStartDate(request.getParameter("startDate")); // 开始时间
+			queryBloodPressureInputBean.setEndDate(request.getParameter("endDate"));// 结束时间
+			String bloodType = request.getParameter("bloodType");
+			if(CodeConstant.BLOOD_TYPE.equals(bloodType)){
+				// 历史的场合
+				queryBloodPressureInputBean.setBloodType(CodeConstant.BLOOD_TYPE_STATUS);
+			}else{
+				// 告警的场合
+				queryBloodPressureInputBean.setBloodType(CodeConstant.ECG_TYPE_STATUS);
+			}
+			queryBloodPressureInputBean.setPointerStart(NumberUtils.toInt(request.getParameter("pointerStart")));
+			queryBloodPressureInputBean.setPageSize(NumberUtils.toInt(request.getParameter("pageSize")));
+			
+	        // 从数据库中取得需要的对象
+	        bloodPressureList = bloodPressureHistoryReadMapper.selectBloodPressureByCondition(queryBloodPressureInputBean);
 	        
 	        ModelMap modelMap = new ModelMap();
 	        
@@ -51,6 +69,14 @@ public class QueryBloodPressureListAction extends ActionSupport{
 	        
 	        modelMap.setRecordTotal(bloodPressureList.size());
 		
+	        // 将java对象转成json对象
+			HttpServletResponse response=ServletActionContext.getResponse();  
+	        //以下代码从JSON.java中拷过来的  
+	        response.setContentType("text/html");  
+	        PrintWriter out;  
+	        out = response.getWriter();  
+	        
+	        // 将java对象转成json对象
 	        JSONObject jsonObject=JSONObject.fromObject(modelMap);//将list 转换为json 数组
 			out.print(jsonObject);
 			out.flush();
