24a90f0431bbbcb3ab472f28505ce85cbe615747
yuting0787 <626808708@qq.com>
2016-01-11 18:11:00 +0800

    做成

---
+++ src/main/java/com/gzjky/action/acitonCommon/ModelMap.java
@@ -0,0 +1,27 @@
+package com.gzjky.action.acitonCommon;
+
+import java.util.List;
+
+public class ModelMap {
+	
+	public int recordTotal;
+	
+	public List<?> outBeanList;
+
+	public int getRecordTotal() {
+		return recordTotal;
+	}
+
+	public void setRecordTotal(int recordTotal) {
+		this.recordTotal = recordTotal;
+	}
+
+	public List<?> getOutBeanList() {
+		return outBeanList;
+	}
+
+	public void setOutBeanList(List<?> outBeanList) {
+		this.outBeanList = outBeanList;
+	}
+
+}
