56892d09d13073cae4e446f2ea804f538e21b1fe
zmj <zmj@zmj-PC>
2016-01-15 14:28:10 +0800

    ！

--- src/main/java/com/gzjky/action/login/LoginAction.java
+++ src/main/java/com/gzjky/action/login/LoginAction.java
@@ -3,7 +3,6 @@ package com.gzjky.action.login;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import org.apache.struts2.ServletActionContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
